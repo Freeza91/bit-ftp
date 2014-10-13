3b2a8e8f84dd3e5b10dcc258f580c15d6e187363
hunter <347212291@qq.com>
2014-10-13 20:15:35 +0800

    login by bit-ftp api plan

---
+++ bit_ftp_v1.rb
@@ -0,0 +1,28 @@
+require 'rufus-scheduler'
+require 'net/http'
+require 'uri'
+require 'json'
+
+module Helpers 
+  
+  USERNAME = "xxxxx"
+  PASSWORD = "xxxxx"
+  ACTION = "login"
+
+  def start
+    login
+    s = Rufus::Scheduler.new
+    s.every "5s" do 
+    end
+  end
+
+  def login
+    uri = URI('http://www.bitunion.org/open_api/bu_logging.php')
+    res = Net::HTTP.post_form(uri, 'action' => ACTION, 
+                              'username' => USERNAME, 'password' => PASSWORD)
+    puts res.body.to_json
+  end
+
+end
+
+
