b1285743d8f34e30f65a2f15e5b05e24ec034db4
hunter <347212291@qq.com>
2014-10-14 20:37:11 +0800

    add v2

--- app.rb
+++ app.rb
@@ -3,17 +3,25 @@
 require 'sinatra'
 require 'rufus-scheduler'
 require 'active_support/all'
-
-Time.zone = "Beijing"
-
-#require './bit_ftp_v2.rb' #使用手动添加cookie
+require './bit_ftp_v2.rb' #使用手动添加cookie
 #require './bit_ftp_v1.rb' #使用联盟开放API，但因暂未开放更新帖子API无法继续开发
 
 #include V1 #V1
-#include V2 #v2
+include V2 #v2
+
+Time.zone = "Beijing"
+T = Time.now + 365 * 24 * 3600
 
 s = Rufus::Scheduler.new
-s.every "60s" do 
-   get_index
+s.every "58m", :first_time => Time.now, :last_at => T do 
+  t = Time.now
+  if t.hour >= 0 and t.hour < 8
+    # do nothing, becsuce at the time nobody can awake!
+  else 
+    get_index
+  end
 end
 
+get '/' do 
+  Time.now
+end
